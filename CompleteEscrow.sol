ac8467fbba9617c7f2abdac36471d9b4a3729351
Momimus <muhayminpirzada@gmail.com>
2026-01-01 16:53:16 +0500

    Complete Deposit Logic (Working)

---
+++ CompleteEscrow.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+contract EscrowStep1 {
+    
+    // State variables (1)
+    address payable public owner;
+    address payable public receiver;
+    uint256 public amount;
+    bool public funded;
+
+    // Event (2)
+    event Deposited(address indexed from, uint256 value);
+
+    // Modifier (3)
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only owner allowed");
+        _;
+    }
+
+    // constructor (4)
+    constructor(address payable _receiver) payable {
+        require(_receiver != address(0), "Invalid receiver");
+
+        owner = payable(msg.sender);
+        receiver = _receiver;
+    }
+
+    // Deposit Function (5)
+    function deposit() external payable onlyOwner {
+        require(!funded, "Already funded");
+        require(msg.value > 0, "Send ETH");
+
+        amount = msg.value;
+        funded = true;
+
+        emit Deposited(msg.sender, msg.value);
+    }
+}
